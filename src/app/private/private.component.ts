2918cc0bdf0d5edf90f8b403203c1101ac9fdf8a
leonidkorzhak <72868914+leonidkorzhak@users.noreply.github.com>
2023-03-21 18:55:23 +0200

    add changes

--- src/app/private/private.component.ts
+++ src/app/private/private.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-private',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink],
+  imports: [CommonModule, RouterOutlet, RouterLink, FooterComponent, SidebarComponent, HeaderComponent],
   templateUrl: './private.component.html',
   styleUrls: ['./private.component.scss']
 })
